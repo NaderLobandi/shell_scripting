1ccd81294f818adb7458114a2148f34543b021f3
NaderLobandi <naderlobandi11@gmail.com>
2026-07-07 20:19:34 -0600

    Add killsleep.sh

---
+++ killsleep.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+$(sleep 30) &
+sleepid1=$!
+
+$(sleep 60) &
+sleepid2=$!
+
+echo "Sleeping pid is $sleepid1"
+echo "Second Sleeping pid is $sleepid2"
+
+sleep 5
+
+$(kill $sleepid1)
+
+$(kill -0 $sleepid1)
+result1=$?
+$(kill -0 $sleepid2)
+result2=$?
+
+echo $result1
+echo $result2
