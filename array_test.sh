05da4b166f860948b12b47d4d6520f30b43221ac
NaderLobandi <naderlobandi11@gmail.com>
2026-07-09 19:40:43 -0600

    Add the happy to sloppy

---
+++ array_test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+MYARRAY=("You cannot shake hands with a clenched fist." "Whoever is happy will make other happy too." "Let us be grateful to peopl who make us happy." "Very little is needed to make a happy life." "Be happy for this moment. This moment is your life.")
+echo ${MYARRAY[1]//happy/sloppy}
+echo ${MYARRAY[2]//happy/sloppy}
+echo ${MYARRAY[3]//happy/sloppy}
+echo ${MYARRAY[4]//happy/sloppy}
+echo ${MYARRAY[5]//happy/sloppy}
